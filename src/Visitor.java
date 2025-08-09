c8ce6c789c8cdd3f3a5c75c74cf8837141f0dee1
IT21195884 <nelithhansindu@gmail.com>
2025-08-10 00:23:05 +0800

    Visitor.java class drafted.

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,34 +1,29 @@
-public class Visitor {
+// PART 1: Visitor extends Person
+public class Visitor extends Person {
+    // At least 2 instance variables for a theme park member/visitor
+    private String visitorId;
+    private String ticketType; // e.g., "Standard", "VIP"
 
-    // PART 1: Visitor extends Person
-    public class Visitor extends Person {
-        // At least 2 instance variables for a theme park member/visitor
-        private String visitorId;
-        private String ticketType; // e.g., "Standard", "VIP"
+    // Default constructor
+    public Visitor() {}
 
-        // Default constructor
-        public Visitor() {}
-
-        // Full constructor (includes Person fields)
-        public Visitor(String firstName, String lastName, int age,
-                       String visitorId, String ticketType) {
-            super(firstName, lastName, age);
-            this.visitorId = visitorId;
-            this.ticketType = ticketType;
-        }
-
-        // Getters & setters
-        public String getVisitorId() { return visitorId; }
-        public void setVisitorId(String visitorId) { this.visitorId = visitorId; }
-
-        public String getTicketType() { return ticketType; }
-        public void setTicketType(String ticketType) { this.ticketType = ticketType; }
-
-        @Override
-        public String toString() {
-            return "Visitor{" + getFullName() + ", id=" + visitorId + ", ticket=" + ticketType + "}";
-        }
+    // Full constructor (includes Person fields)
+    public Visitor(String firstName, String lastName, int age,
+                   String visitorId, String ticketType) {
+        super(firstName, lastName, age);
+        this.visitorId = visitorId;
+        this.ticketType = ticketType;
     }
 
+    // Getters & setters
+    public String getVisitorId() { return visitorId; }
+    public void setVisitorId(String visitorId) { this.visitorId = visitorId; }
+
+    public String getTicketType() { return ticketType; }
+    public void setTicketType(String ticketType) { this.ticketType = ticketType; }
+
+    @Override
+    public String toString() {
+        return "Visitor{" + getFullName() + ", id=" + visitorId + ", ticket=" + ticketType + "}";
+    }
 }
-//(1)
