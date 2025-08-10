017f63a1f04e628ac0d6665a911822c53dc24e0a
IT21195884 <nelithhansindu@gmail.com>
2025-08-10 11:03:22 +0800

    Person.java file improved and add explaining comments.

--- src/Person.java
+++ src/Person.java
@@ -1,32 +1,40 @@
-// PART 1 & 2: Abstract base class (never directly instantiated)
+//************************** PART 1 & 2: Abstract base class (never directly instantiated)
 public abstract class Person {
-    // At least 3 instance variables for a person
-    private String firstName;
+
+    private String firstName;         //Declaring 3 variables
     private String lastName;
     private int age;
 
-    // Default constructor
-    public Person() {}
 
-    // Full constructor
+    public Person() {}     //Default Constructor
+
+    // Second/Parameterized Constructor
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName  = lastName;
         this.age       = age;
     }
 
-    // Getters & setters
-    public String getFirstName() { return firstName; }
+    // Getters & setters for each declared variables
+    public String getFirstName() {
+        return firstName; }
+
     public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    public String getLastName() { return lastName; }
+    public String getLastName() {
+        return lastName; }
+
     public void setLastName(String lastName) { this.lastName = lastName; }
 
-    public int getAge() { return age; }
+    public int getAge() {
+        return age; }
+
     public void setAge(int age) { this.age = age; }
 
+    //Return full name as a Single string
     public String getFullName() { return firstName + " " + lastName; }
 
+    //toString method to called when printing objects
     @Override
     public String toString() {
         return getFullName() + " (age " + age + ")";
